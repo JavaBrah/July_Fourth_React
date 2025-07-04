c57ae642e656e8db1702654e16517f9ed7848802
Kegan <kegcliffjohnson@gmail.com>
2025-07-04 10:13:26 -0400

    Added react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import FileNotFound from './pages/FileNotFound.jsx'
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  errorElement: <FileNotFound />
+}]);
+
+
+
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+  
+);
